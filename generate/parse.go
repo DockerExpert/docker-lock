63ef59e75fc01c2138bb8bff070154ac25b6bb75
Michael Perel <michaelsethperel@gmail.com>
2019-08-02 18:38:53 +0000

    more concurrency

--- generate/parse.go
+++ generate/parse.go
@@ -12,153 +12,198 @@ import (
 type parsedImageLine struct {
 	line     string
 	fileName string
+	err      error
 }
 
 func parseComposefiles(fileNames []string) ([]parsedImageLine, error) {
-	parsedImageLines := make([]parsedImageLine, 0)
+	parsedImageLines := make(chan parsedImageLine)
+	var numFiles int
 	for _, fileName := range fileNames {
-		fileName = "docker-compose.yml"
-		yamlByt, err := ioutil.ReadFile(fileName)
-		if err != nil {
-			return nil, err
-		}
-		var comp map[string]interface{}
-		if err := yaml.Unmarshal(yamlByt, &comp); err != nil {
-			return nil, err
-		}
-		services := comp["services"].(map[interface{}]interface{})
-		for _, serviceConfig := range services {
-			var result parsedImageLine
-			result.fileName = fileName
-			config := serviceConfig.(map[interface{}]interface{})
-			if _, ok := config["build"]; !ok {
-				imageName, _ := config["image"].(string)
-				result.line = os.ExpandEnv(imageName)
-				parsedImageLines = append(parsedImageLines, result)
-				continue
+		numFiles++
+		go func(fileName string) {
+			yamlByt, err := ioutil.ReadFile(fileName)
+			if err != nil {
+				parsedImageLines <- parsedImageLine{err: err}
+				return
 			}
-			switch build := config["build"].(type) {
-			case string:
-				if build != "" {
-					build = os.ExpandEnv(build)
-					fi, err := os.Stat(build)
-					if err != nil {
-						return nil, err
-					}
-					mode := fi.Mode()
-					if mode.IsDir() {
-						results, err := parseDockerfiles([]string{path.Join(build, "Dockerfile")}, nil)
+			var comp map[string]interface{}
+			if err := yaml.Unmarshal(yamlByt, &comp); err != nil {
+				parsedImageLines <- parsedImageLine{err: err}
+				return
+			}
+			services := comp["services"].(map[interface{}]interface{})
+			for _, serviceConfig := range services {
+				var result parsedImageLine
+				result.fileName = fileName
+				config := serviceConfig.(map[interface{}]interface{})
+				if _, ok := config["build"]; !ok {
+					imageName, _ := config["image"].(string)
+					result.line = os.ExpandEnv(imageName)
+					parsedImageLines <- result
+					return
+				}
+				switch build := config["build"].(type) {
+				case string:
+					if build != "" {
+						build = os.ExpandEnv(build)
+						fi, err := os.Stat(build)
 						if err != nil {
-							return nil, err
+							parsedImageLines <- parsedImageLine{err: err}
+							return
 						}
-						parsedImageLines = append(parsedImageLines, results...)
-						continue
+						mode := fi.Mode()
+						if mode.IsDir() {
+							results, err := parseDockerfiles([]string{path.Join(build, "Dockerfile")}, nil)
+							if err != nil {
+								parsedImageLines <- parsedImageLine{err: err}
+							}
+							for _, result := range results {
+								parsedImageLines <- result
+							}
+							return
+						} else {
+							results, err := parseDockerfiles([]string{build}, nil)
+							if err != nil {
+								parsedImageLines <- parsedImageLine{err: err}
+							}
+							for _, result := range results {
+								parsedImageLines <- result
+							}
+							return
+						}
+					}
+				case map[interface{}]interface{}:
+					context := build["context"].(string)
+					context = os.ExpandEnv(context)
+					dockerfileName, _ := build["dockerfile"].(string)
+					dockerfileName = os.ExpandEnv(dockerfileName)
+					var dockerfile string
+					if dockerfileName == "" {
+						dockerfile = path.Join(context, "Dockerfile")
 					} else {
-						results, err := parseDockerfiles([]string{build}, nil)
+						dockerfile = path.Join(context, dockerfileName)
+					}
+					args, _ := build["args"].([]interface{})
+					argsMap := make(map[string]string)
+					if len(args) == 0 {
+						results, err := parseDockerfiles([]string{dockerfile}, nil)
 						if err != nil {
-							return nil, err
+							parsedImageLines <- parsedImageLine{err: err}
+							return
 						}
-						parsedImageLines = append(parsedImageLines, results...)
-						continue
+						for _, result := range results {
+							parsedImageLines <- result
+						}
+						return
+					} else {
+						for _, arg := range args {
+							argString := os.ExpandEnv(arg.(string))
+							argsSlice := strings.Split(argString, "=")
+							argsMap[argsSlice[0]] = argsSlice[1]
+						}
+						results, err := parseDockerfiles([]string{dockerfile}, argsMap)
+						if err != nil {
+							parsedImageLines <- parsedImageLine{err: err}
+							return
+						}
+						for _, result := range results {
+							parsedImageLines <- result
+						}
+						return
 					}
-				}
-			case map[interface{}]interface{}:
-				context := build["context"].(string)
-				context = os.ExpandEnv(context)
-				dockerfileName, _ := build["dockerfile"].(string)
-				dockerfileName = os.ExpandEnv(dockerfileName)
-				var dockerfile string
-				if dockerfileName == "" {
-					dockerfile = path.Join(context, "Dockerfile")
-				} else {
-					dockerfile = path.Join(context, dockerfileName)
-				}
-				args, _ := build["args"].([]interface{})
-				argsMap := make(map[string]string)
-				if len(args) == 0 {
-					results, err := parseDockerfiles([]string{dockerfile}, nil)
-					if err != nil {
-						return nil, err
-					}
-					parsedImageLines = append(parsedImageLines, results...)
-					continue
-				} else {
-					for _, arg := range args {
-						argString := os.ExpandEnv(arg.(string))
-						argsSlice := strings.Split(argString, "=")
-						argsMap[argsSlice[0]] = argsSlice[1]
-					}
-					results, err := parseDockerfiles([]string{dockerfile}, argsMap)
-					if err != nil {
-						return nil, err
-					}
-					parsedImageLines = append(parsedImageLines, results...)
-					continue
 				}
 			}
-		}
+		}(fileName)
 	}
-	return parsedImageLines, nil
+	parsedImageLinesSlice := make([]parsedImageLine, 0)
+	for {
+		if numFiles == 0 {
+			break
+		}
+		parsedImageLine := <-parsedImageLines
+		if parsedImageLine.err != nil {
+			return nil, parsedImageLine.err
+		}
+		parsedImageLinesSlice = append(parsedImageLinesSlice, parsedImageLine)
+		numFiles--
+	}
+	return parsedImageLinesSlice, nil
 }
 
 func parseDockerfiles(fileNames []string, buildArgs map[string]string) ([]parsedImageLine, error) {
-	parsedImageLines := make([]parsedImageLine, 0)
+	parsedImageLines := make(chan parsedImageLine)
+	var numFiles int
 	for _, fileName := range fileNames {
-		dockerfile, err := os.Open(fileName)
-		if err != nil {
-			return nil, err
-		}
-		defer dockerfile.Close()
-		stageNames := make(map[string]bool)
-		buildVars := make(map[string]string)
-		scanner := bufio.NewScanner(dockerfile)
-		scanner.Split(bufio.ScanLines)
-		for scanner.Scan() {
-			fields := strings.Fields(scanner.Text())
-			if len(fields) > 0 {
-				switch instruction := strings.ToLower(fields[0]); instruction {
-				case "arg", "env":
-					//INSTRUCTION VAR1=VAL1 VAR2=VAL2 ...
-					if strings.Contains(fields[1], "=") {
-						for _, pair := range fields[1:] {
-							splitPair := strings.Split(pair, "=")
-							key, val := splitPair[0], splitPair[1]
+		numFiles++
+		go func(fileName string) {
+			dockerfile, err := os.Open(fileName)
+			if err != nil {
+				parsedImageLines <- parsedImageLine{err: err}
+			}
+			defer dockerfile.Close()
+			stageNames := make(map[string]bool)
+			buildVars := make(map[string]string)
+			scanner := bufio.NewScanner(dockerfile)
+			scanner.Split(bufio.ScanLines)
+			for scanner.Scan() {
+				fields := strings.Fields(scanner.Text())
+				if len(fields) > 0 {
+					switch instruction := strings.ToLower(fields[0]); instruction {
+					case "arg", "env":
+						//INSTRUCTION VAR1=VAL1 VAR2=VAL2 ...
+						if strings.Contains(fields[1], "=") {
+							for _, pair := range fields[1:] {
+								splitPair := strings.Split(pair, "=")
+								key, val := splitPair[0], splitPair[1]
+								buildVars[key] = val
+							}
+						} else if len(fields) == 3 {
+							//INSTUCTION VAR1 VAL1
+							key, val := fields[1], fields[2]
 							buildVars[key] = val
+						} else if instruction == "arg" && len(fields) == 2 {
+							// ARG VAR1
+							argName := fields[1]
+							if argVal, ok := buildArgs[argName]; ok {
+								buildVars[argName] = argVal
+							}
 						}
-					} else if len(fields) == 3 {
-						//INSTUCTION VAR1 VAL1
-						key, val := fields[1], fields[2]
-						buildVars[key] = val
-					} else if instruction == "arg" && len(fields) == 2 {
-						// ARG VAR1
-						argName := fields[1]
-						if argVal, ok := buildArgs[argName]; ok {
-							buildVars[argName] = argVal
+					case "from":
+						line := expandBuildVars(fields[1], buildVars)
+						// each from resets buildvars
+						buildVars = make(map[string]string)
+						// guarding against the case where the line is the name of a previous build stage
+						// rather than a base image.
+						// For instance, FROM <previous-stage> AS <name>
+						if !stageNames[line] {
+							parsedImageLines <- parsedImageLine{line: line, fileName: fileName}
 						}
-					}
-				case "from":
-					line := expandBuildVars(fields[1], buildVars)
-					// each from resets buildvars
-					buildVars = make(map[string]string)
-					// guarding against the case where the line is the name of a previous build stage
-					// rather than a base image.
-					// For instance, FROM <previous-stage> AS <name>
-					if !stageNames[line] {
-						result := parsedImageLine{line: line, fileName: fileName}
-						parsedImageLines = append(parsedImageLines, result)
-					}
-					// multistage build
-					// FROM <image> AS <name>
-					// FROM <previous-stage> as <name>
-					if len(fields) == 4 {
-						stageName := expandBuildVars(fields[3], buildVars)
-						stageNames[stageName] = true
+						// multistage build
+						// FROM <image> AS <name>
+						// FROM <previous-stage> as <name>
+						if len(fields) == 4 {
+							stageName := expandBuildVars(fields[3], buildVars)
+							stageNames[stageName] = true
+						}
 					}
 				}
 			}
-		}
+		}(fileName)
 	}
-	return parsedImageLines, nil
+	parsedImageLinesSlice := make([]parsedImageLine, 0)
+	for {
+		if numFiles == 0 {
+			break
+		}
+		parsedImageLine := <-parsedImageLines
+		if parsedImageLine.err != nil {
+			return nil, parsedImageLine.err
+		}
+		parsedImageLinesSlice = append(parsedImageLinesSlice, parsedImageLine)
+		numFiles--
+	}
+	return parsedImageLinesSlice, nil
+
 }
 
 func expandBuildVars(line string, buildVars map[string]string) string {
